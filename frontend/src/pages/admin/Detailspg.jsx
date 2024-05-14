703714666a18cf0a504a818a9cf4e3a6ae885605
gilbertmunuotz <officialgibert24@gmail.com>
2024-05-14 21:20:16 +0300

    working on Update logic

--- frontend/src/pages/admin/Detailspg.jsx
+++ frontend/src/pages/admin/Detailspg.jsx
@@ -1,26 +1,32 @@
 import { FaTh } from "react-icons/fa";
+import { toast } from "react-toastify";
 import { CiLogout } from "react-icons/ci";
-import { NavLink, } from 'react-router-dom';
 import { useState, useEffect } from "react";
 import { useParams } from 'react-router-dom';
 import Spinner from "../../components/Spinner";
 import { IoIosAddCircleOutline } from "react-icons/io";
-// import { toast } from 'react-toastify';
+import { NavLink, useNavigate } from 'react-router-dom';
 
 function Detailspg() {
+  //SideBar Components
   const menuItems = [
     { path: "/adminH", name: "Home", icon: <FaTh className="ml-1" /> },
     { path: "/addItems", name: "Add Product", icon: <IoIosAddCircleOutline size={24} /> },
   ];
 
   const { id } = useParams();
+  const navigate = useNavigate();
   const [productData, setProductData] = useState(null);
   const [name, setName] = useState('');
   const [category, setCategory] = useState('');
   const [description, setDescription] = useState('');
   const [price, setPrice] = useState('');
+  const [image, setImage] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const categoryOptions = ["Computing", "Networking", "Stationeries"];
 
+
+  //Get A Single  Product By Id Logic
   useEffect(() => {
     const fetchData = async () => {
       if (!id) return; // Exit early if id is not available
@@ -28,6 +34,7 @@ function Detailspg() {
       try {
         setIsLoading(true)
         const url = `http://localhost:3001/api/product/${id}`;
+
         const response = await fetch(url, { method: 'GET' });
 
         if (!response.ok) {
@@ -36,6 +43,10 @@ function Detailspg() {
 
         const data = await response.json();
         setProductData(data);
+        setName(data.name);
+        setCategory(data.category);
+        setDescription(data.description);
+        setPrice(data.price);
       } catch (error) {
         console.error('Error fetching data:', error);
       } finally {
@@ -46,6 +57,49 @@ function Detailspg() {
     fetchData();
 
   }, [id]); // Re-run useEffect when id changes
+
+
+  //Handle Update Logic
+  async function handleUpdate(event) {
+    event.preventDefault();
+
+    const updatingFields = { name, category, description, price, };
+
+    const formData = new FormData();
+    formData.append('data', JSON.stringify(updatingFields));
+
+    if (image) {
+      formData.append('image', image);
+    }
+    console.log(formData);
+
+    const url = `http://localhost:3001/api/updateProduct/${id}`;
+    try {
+      const response = await fetch(url, {
+        method: 'PUT',
+        body: formData,
+        headers: { 'Content-Type': 'form-data', },
+      });
+
+      if (!response) {
+        throw new Error('Update failed!');
+      } else {
+        // eslint-disable-next-line no-unused-vars
+        const updatedProductData = await response.json();
+        navigate('/adminH')
+        toast.success('Product Updated Succesfully!');
+      }
+    } catch (error) {
+      console.error('Error updating product:', error);
+      toast.error('Error Updating Product');
+    }
+  }
+
+
+  //Capture Image
+  async function handleImage(event) {
+    setImage(event.target.files[0]);
+  }
 
   return (
     <>
@@ -61,21 +115,19 @@ function Detailspg() {
               </NavLink>
             ))}
 
-            <button type="button" className="mt-auto mb-10 cursor-not-allowed" disabled={true}>
+            <button type="button" className="mt-auto mb-10 cursor-not-allowed">
               <CiLogout className="inline-block mr-3" size={24} />Log Out
             </button>
           </div>
         </div>
 
         <main className="flex flex-col ml-64 text-center h-screen justify-center items-center">
-          <form>
+          <form onSubmit={handleUpdate}>
             {productData ? (
               <>
                 <div className="flex flex-col items-center">
                   <img src={`/${productData.imageURL}`} alt={productData.name} className="h-80 w-96 object-fill rounded-t-xl" />
-                  <button type="button" className="bg-sky-400 py-2 px-3 my-2 rounded-lg">
-                    Upload Image
-                  </button>
+                  <input type="file" className="mt-3" onChange={handleImage} />
                 </div>
 
                 <div className="grid grid-cols-2 gap-4 ml-36">
@@ -83,43 +135,53 @@ function Detailspg() {
                     <label className="mr-4">Name:</label>
                     <input
                       type="text"
-                      value={productData.name}
+                      value={name}
                       onChange={(event) => setName(event.target.value)}
                     />
+
                     <label className="mr-4 mt-4">Description:</label>
                     <textarea
                       type="text"
-                      value={productData.description}
+                      value={description}
                       onChange={(event) => setDescription(event.target.value)}
                       cols="30"
                     />
                   </div>
+
                   <div className="block">
                     <label className="mr-4">Category:</label>
-                    <input
-                      type="text"
-                      value={productData.category}
-                      onChange={(event) => setCategory(event.target.value)}
-                    />
+                    <select
+                      className="block p-2 rounded-md border border-gray-300  focus:border-sky-500"
+                      value={category}
+                      onChange={(event) => setCategory(event.target.value)}  >
+                      {categoryOptions.map((option) => (
+                        <option key={option} value={option}>
+                          {option}
+                        </option>
+                      ))}
+                    </select>
+
+
                     <label className="mr-4 mt-4">Price:</label>
                     <input
                       type="number"
-                      value={productData.price}
+                      value={price}
                       onChange={(event) => setPrice(event.target.value)}
                     />
+
                   </div>
                 </div>
-
-
               </>
             ) : (
               <Spinner loading={isLoading} />
             )}
-          </form>
 
-          <div>
-            <button type="button" className="bg-sky-500 m-3 p-2 rounded-lg">Update</button>
-          </div>
+            <div>
+              <button type="submit" disabled={isLoading} className="bg-sky-500 m-3 p-2 rounded-lg">
+                {isLoading ? 'Updating...' : 'Update'}
+              </button>
+            </div>
+          </form>
         </main>
       </div>
 
