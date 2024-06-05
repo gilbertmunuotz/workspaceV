1d3f54166a5e0590d9ddfc392bf6cb5d6257d902
gilbertmunuotz <128461895+gilbertmunuotz@users.noreply.github.com>
2024-06-05 09:42:47 +0300

    Updated apiSlice.js

--- frontend/src/auth/apiSlice.js
+++ frontend/src/auth/apiSlice.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const USERS_URL = '/api';
+const USERS_URL = 'https://workspace-vb.vercel.app/api';
 
 const baseQuery = fetchBaseQuery({
     baseUrl: USERS_URL, // Assuming all your backend API routes start with /api
